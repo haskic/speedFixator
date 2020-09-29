ba3248aec55332dffceac88569d9522abdfb99f1
Alexander Speek <kot3sany@gmail.com>
2020-09-29 09:54:37 +0300

    added Fixation model

---
+++ Models/Fixation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpeedFixator.Models
+{
+    public class Fixation
+    {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; }
+        public string VechicleNumber { get; set; }
+        public double Speed { get; set; }
+    }
+}
