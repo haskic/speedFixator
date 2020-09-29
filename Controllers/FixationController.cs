a5ae8e9952ddd5d2f4cf57702c05ab26262ee6b8
Alexander Speek <kot3sany@gmail.com>
2020-09-29 10:05:07 +0300

    added FixationController

---
+++ Controllers/FixationController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using SpeedFixator.Models;
+
+namespace SpeedFixator.Controllers
+{
+    [ApiController]
+    public class FixationController : ControllerBase
+    {
+        private readonly ILogger<FixationController> _logger;
+        private readonly IFixationRepository _fixationRepository;
+        private readonly IConfiguration _configuration;
+        public FixationController(IConfiguration configuration, ILogger<FixationController> logger, IFixationRepository fixationRepository)
+        {
+            this._logger = logger;
+            this._fixationRepository = fixationRepository;
+            this._configuration = configuration;
+        }
+
+
+
+    }
+}
